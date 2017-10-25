0469c886d819e4d58a05b7d29fa9b3e4307e8126
jworsf01 <32646818+jworsf01@users.noreply.github.com>
2017-10-25 19:05:33 +0100

    Finished Lunar Lander 1.0 and added data

--- LunarLander.py
+++ LunarLander.py
@@ -1,66 +1,81 @@
 '''Joseph Worsfold - jworsf01.
-   Then your description of what the program does.'''
+   This is the Lunar Lander (1.0) game which has been developed to be played
+   multiple times within specific parameters. These parameters include the
+   following: altitude, velocity, fuel, gravity, constant, burn. Each turn
+   the player is asked how much fuel to burn, which will affect the downward
+   velocity created by gravity, and this will affect the altitude on the next
+   turn. Winners must get to zero or less altitude at a velociy under 10 metres
+   per second. Extra points if it's under 5 metres per second.'''
 
-# The game is not timed: 1 turn = 1 second
-# Keep track of the number of turns/seconds to land
-# Variables are Altitude/Velocity/Fuel
-# Values to start 1000.0/0.0/1000.0
+new_game = True
 
-# User specify how much fuel to burn each turn
-# Zero is legal - if asked more burn all - negative = 0
+while new_game:
+    ready_player = input('Are you ready to play (again)? (Y/N): ')
+    altitude = 500.0
+    velocity = 0.0
+    fuel = 500.0
+    turn = 0
+    gravity = 2.0
+    constant = 0.15
+    if ready_player == 'Y' or ready_player == 'y':
+        while altitude > 0:
+            print('------------------------------------------------')
+            print('Turn: ' + str(turn))
+            print('Altitude: ' + str(round(altitude, 1)) + ' metres')
+            print('Velocity: ' + str(round(velocity, 1)) + ' metres/second')
+            print('Fuel: ' + str(round(fuel, 1)) + ' litres')
+            print('------------------------------------------------')
 
-# Game ends when your altitude becomes zero or less
-# A safe landing occurs if your speed is under 10m/s
+            if fuel <= 0:
+                print('Whoops you\'re out of fuel')
+                burn = 0
+            else:
+                burn = float(input('How much fuel do you want to burn?: '))
 
-# Each turn velocity increases by 1.6 metres/seconds
-# Decreases by proportional amount to fuel burn by some constant - start with 0.15
-# Altitude decreases by your velocity multiplied by amount of time...
-# fuel decreases by the amount you burn, or total if requesting more
+            if burn < 0:
+                burn = 0
+            if burn > fuel:
+                burn = fuel
 
-# Determine if you have landed
-# Altitude is zero or less
-# If successful print altitude of zero and final numbers
-# if crash, use velocity to print out how deep a crater you have mde is
-# after each game ask if the user wants to play again
+            fuel -= burn
+            velocity += gravity
+            velocity -= constant * burn
+            altitude -= velocity
+            turn += 1
 
-altitude = 1000.0
-velocity = 0.0
-fuel = 1000.0
-turn = 0
-gravity = 1.6
-constant = 0.15
+        print('------------------------------------------------')
+        print('Turn: ' + str(turn))
+        print('Altitude: ' + str(round(altitude, 1)) + ' metres')
+        print('Velocity: ' + str(round(velocity, 1)) + ' metres/second')
+        print('Fuel: ' + str(round(fuel, 1)) + ' litres')
+        print('------------------------------------------------')
 
-while altitude > 0:
-    print('----------------')
-    print('Turn: ' + str(turn))
-    print('Altitude: ' + str(round(altitude, 1)) + ' metres')
-    print('Velocity: ' + str(round(velocity, 1)) + ' metres/second')
-    print('Fuel: ' + str(round(fuel, 1)) + ' litres')
-    print('----------------')
-
-    if fuel <= 0:
-        print('Whoops you\'re out of fuel')
-        burn = 0
-    else:
-        burn = float(input('How much fuel do you want to burn?: '))
-    if burn < 0:
-        burn = 0
-    if burn > fuel:
-        burn = fuel
-
-    fuel -= burn
-    velocity += gravity
-    velocity -= constant * burn
-    altitude -= velocity
-    turn += 1
+        if velocity > 10.0:
+            print('You\'ve crashed. Creating a two mile crater.')
+        elif 10.0 >= velocity > 5.0:
+            print('You\'ve landed hard. Marooned on the moon.')
+            print('------------------------------------------------')
+            print('Turn: ' + str(turn))
+            print('Altitude: ' + str(round(altitude, 1)) + ' metres')
+            print('Velocity: ' + str(round(velocity, 1)) + ' metres/second')
+            print('Fuel: ' + str(round(fuel, 1)) + ' litres')
+        else:
+            print('You\'ve landed safely')
+            print('------------------------------------------------')
+            altitude = 0.0
+            print('Turn: ' + str(turn))
+            print('Altitude: ' + str(round(altitude, 1)) + ' metres')
+            print('Velocity: ' + str(round(velocity, 1)) + ' metres/second')
+            print('Fuel: ' + str(round(fuel, 1)) + ' litres')
     
-print('----------------')
-print('Landed or Crashed...')
-print('----------------')
-print('Turn: ' + str(turn))
-print('Altitude: ' + str(round(altitude, 1)) + ' metres')
-print('Velocity: ' + str(round(velocity, 1)) + ' metres/second')
-print('Fuel: ' + str(round(fuel, 1)) + ' litres')
-print('----------------')
+        print('------------------------------------------------')
+        print('            GAME OVER')
+        print('------------------------------------------------')
+
+    elif ready_player == 'N' or ready_player == 'n':
+        print('Thanks for playing.')
+        new_game = False
+    else:
+        print('Try again...')
 
 
