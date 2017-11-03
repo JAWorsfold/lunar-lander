a1c0446626113ff0d46670ac82f41dba13e50c97
jworsf01 <32646818+jworsf01@users.noreply.github.com>
2017-11-03 14:36:21 +0000

    Continued working and refracting

--- LunarLander.py
+++ LunarLander.py
@@ -9,27 +9,36 @@
 
 new_game = True
 
+# def start_game(): how do I make it so the first game is fresh...
+# def new_game(): how do I make it start and restart?        
+
+def scoreboard():
+    print('------------------------------------------------')
+    print('Turn: ' + str(turn))
+    print('Altitude: ' + str(round(altitude, 1)) + ' metres')
+    print('Velocity: ' + str(round(velocity, 1)) + ' metres/second')
+    print('Fuel: ' + str(round(fuel, 1)) + ' litres')
+                     
 while new_game:
     ready_player = input('Are you ready to play (again)? (Y/N): ')
+
+    turn = 0
     altitude = 500.0
     velocity = 0.0
     fuel = 500.0
-    turn = 0
     gravity = 2.0
     constant = 0.15
+
     if ready_player == 'Y' or ready_player == 'y':
         while altitude > 0:
-            print('------------------------------------------------')
-            print('Turn: ' + str(turn))
-            print('Altitude: ' + str(round(altitude, 1)) + ' metres')
-            print('Velocity: ' + str(round(velocity, 1)) + ' metres/second')
-            print('Fuel: ' + str(round(fuel, 1)) + ' litres')
-            print('------------------------------------------------')
+            scoreboard()
 
             if fuel <= 0:
+                print('------------------------------------------------')
                 print('Whoops you\'re out of fuel')
                 burn = 0
             else:
+                print('------------------------------------------------')
                 burn = float(input('How much fuel do you want to burn?: '))
 
             if burn < 0:
@@ -43,30 +52,21 @@ while new_game:
             altitude -= velocity
             turn += 1
 
-        print('------------------------------------------------')
-        print('Turn: ' + str(turn))
-        print('Altitude: ' + str(round(altitude, 1)) + ' metres')
-        print('Velocity: ' + str(round(velocity, 1)) + ' metres/second')
-        print('Fuel: ' + str(round(fuel, 1)) + ' litres')
-        print('------------------------------------------------')
-
         if velocity > 10.0:
+            print('------------------------------------------------')
             print('You\'ve crashed. Creating a two mile crater.')
         elif 10.0 >= velocity > 5.0:
-            print('You\'ve landed hard. Marooned on the moon.')
             print('------------------------------------------------')
-            print('Turn: ' + str(turn))
-            print('Altitude: ' + str(round(altitude, 1)) + ' metres')
-            print('Velocity: ' + str(round(velocity, 1)) + ' metres/second')
-            print('Fuel: ' + str(round(fuel, 1)) + ' litres')
-        else:
-            print('You\'ve landed safely')
-            print('------------------------------------------------')
+            print('You\'ve landed hard! You\'re final score is:')
             altitude = 0.0
-            print('Turn: ' + str(turn))
-            print('Altitude: ' + str(round(altitude, 1)) + ' metres')
-            print('Velocity: ' + str(round(velocity, 1)) + ' metres/second')
-            print('Fuel: ' + str(round(fuel, 1)) + ' litres')
+            print('FINAL SCORE:')
+            scoreboard()
+        else:
+            print('------------------------------------------------')
+            print('You\'ve landed safely! You\'re final score is:')
+            altitude = 0.0
+            print('FINAL SCORE:')
+            scoreboard()
     
         print('------------------------------------------------')
         print('            GAME OVER')
@@ -77,5 +77,4 @@ while new_game:
         new_game = False
     else:
         print('Try again...')
-
 
